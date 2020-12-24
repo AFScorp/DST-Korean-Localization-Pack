04604abab3cd3424d66357fdf229fe8ba7eb0216
AFS Co. Ltd <qkrcotlr1@gmail.com>
2020-12-25 03:39:26 +0900

    Update and rename fontlogic.lua to fontapply_wormwood.lua

--- main/fontapply_wormwood.lua
+++ main/fontapply_wormwood.lua
@@ -12,4 +12,6 @@ table.insert(GLOBAL.FONTS, {filename = MODROOT.."fonts/talkingfont_wormwood_kr.z
 AddPrefabPostInit("wormwood", function(inst)
     inst.components.talker.fontsize = 40
     inst.components.talker.font = TALKINGFONT_WORMWOOD_KR
-end)
+end)
+
+--modmain에 직접 옮겨넣거나 modimport 기능을 사용하세요

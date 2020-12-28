92bc1b4d30977d6d03052e5c15f79cb810c12323
AFS Co. Ltd <qkrcotlr1@gmail.com>
2020-12-28 21:20:05 +0900

    Update modinfo.lua

--- modinfo.lua
+++ modinfo.lua
@@ -1,5 +1,5 @@
 -- This information tells other players more about the mod
-name = "한글 모드 [서버 버전]"
+name = "한글 모드 [클라이언트 버전]"
 version = "0.8.34.20200303.00"
 description = "Version : "..version.."\n게임내 기본 폰트만을 이용합니다.\n\n모드 사용시 필요한 설정(게임내 옵션설정)\nWigfrid's Umlauts(위그프리드 움라우트) 옵션 활성화\n저사양 텍스쳐 옵션 비활성화\n일부시스템에서 게임이 튕기는 경우는, 노트북 모드 비활성화\n"
 author = "Mr.Dream"
@@ -17,6 +17,6 @@ reign_of_giants_compatible = false
 shipwrecked_compatible = false
 dst_compatible = true
 
-all_clients_require_mod=true
+all_clients_require_mod=false
 server_filter_tags = {"korean","language"}
-client_only_mod=false
+client_only_mod=true

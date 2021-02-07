c004f759959cef74b44e74bb0e45f02132245e99
박채식 <qkrcotlr1@gmail.com>
2021-02-07 19:46:43 +0900

    some tweaks

--- main/fontapply_wormwood.lua
+++
@@ -1,8 +0,0 @@
-local TALKINGFONT_WORMWOOD_KR = "talkingfont_wormwood_kr"
-
-table.insert(GLOBAL.FONTS, {filename = MODROOT.."fonts/talkingfont_wormwood_kr.zip", alias = TALKINGFONT_WORMWOOD_KR, fallback = GLOBAL.DEFAULT_FALLBACK_TABLE_OUTLINE})  
-
-AddPrefabPostInit("wormwood", function(inst)
-    inst.components.talker.fontsize = 40
-    inst.components.talker.font = TALKINGFONT_WORMWOOD_KR
-end)

32ef6b86e218822fef2721128679713ef6b9c3dd
AFS Co. Ltd <qkrcotlr1@gmail.com>
2021-02-15 00:42:25 +0900

    버전업 및 썸네일 수정

--- modinfos/server/modinfo.lua
+++ modinfos/server/modinfo.lua
@@ -1,8 +1,8 @@
 -- This information tells other players more about the mod
 name = "굶지마 다함께 한글화 [서버 버전]"
-version = "1.0.0.202102011.02"
-description = "Version : "..version.."\n게임 내 기본 폰트만을 이용합니다.\n동굴이 있거나 Dedicated 서버에 적용할 때 사용되는 모드입니다.\n\n모드 사용 시 필요한 설정(게임 내 설정)\nWigfrid's Umlauts(위그프리드의 움라우트) 옵션 활성화\n저사양 텍스쳐 옵션 비활성화\n일부 시스템에서 게임이 튕기는 경우는, 노트북 모드 비활성화\n"
-author = "Mr.Dream, AFS Co. Ltd., Original MOD by wrinos"
+version = "1.0.0.202102015.00"
+description = "Version : "..version.."\n\n게임 내 기본 폰트만을 이용합니다.\n동굴이 있는 서버는 서버 버전을 따로 사용해 주세요.\n\n\n모드 사용 시 필요한 설정(게임 내 설정)\n\n\nWigfrid's Umlauts(위그프리드의 움라우트) 옵션 활성화\n\n저사양 텍스쳐 옵션 비활성화\n\n일부 시스템에서 게임이 튕기는 경우는, 노트북 모드 비활성화\n\n\n\n모드 총책임자: Mr.Dream\n기술 총책임자: AFS Co. Ltd.\nOriginal mod by wrinos"
+author = "굶지마 다함께 한글화 팀"
 forumthread = ""
 
 -- This lets other players know if your mod is out of date, update it to match the current version in the game

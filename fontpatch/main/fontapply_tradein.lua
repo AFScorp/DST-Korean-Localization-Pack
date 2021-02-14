4fb66579f067a3754f827ff59127bcc9df628979
AFS Co. Ltd <qkrcotlr1@gmail.com>
2021-02-15 00:42:10 +0900

    폰트 크기 조절 및 폰트 미적용 대상 적용
    제곧내

--- fontpatch/main/fontapply_tradein.lua
+++ fontpatch/main/fontapply_tradein.lua
@@ -9,6 +9,11 @@ local TALKINGFONT_TRADEIN_KR = "talkingfont_tradein_kr"
 
 table.insert(GLOBAL.FONTS, {filename = MODROOT.."fonts/talkingfont_tradein_kr.zip", alias = TALKINGFONT_TRADEIN_KR, fallback = fallback_tradein})  
 
+
 AddPrefabPostInit("yotb_stage", function(inst)
     inst.components.talker.font = TALKINGFONT_TRADEIN_KR
-end)
+end)
+AddPrefabPostInit("yotb_stage_voice", function(inst)
+    inst.components.talker.font = TALKINGFONT_TRADEIN_KR
+end)
+
--- fontpatch/main/fontapply_wormwood.lua
+++ fontpatch/main/fontapply_wormwood.lua
@@ -10,6 +10,6 @@ local TALKINGFONT_WORMWOOD_KR = "talkingfont_wormwood_kr"
 table.insert(GLOBAL.FONTS, {filename = MODROOT.."fonts/talkingfont_wormwood_kr.zip", alias = TALKINGFONT_WORMWOOD_KR, fallback = fallback_wormwood})  
 
 AddPrefabPostInit("wormwood", function(inst)
-    inst.components.talker.fontsize = 40
+    inst.components.talker.fontsize = 35
     inst.components.talker.font = TALKINGFONT_WORMWOOD_KR
 end)

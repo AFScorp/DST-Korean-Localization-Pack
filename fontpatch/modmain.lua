8271f39afa250f12eed640ff030e9c864ea34350
AFS Co. Ltd <qkrcotlr1@gmail.com>
2021-03-07 01:17:27 +0900

    글꼴 불러오기 메소드 안정화

--- fontpatch/modmain.lua
+++ fontpatch/modmain.lua
@@ -1,68 +1,89 @@
+local _G = GLOBAL
+local TheSim = _G.TheSim
+
 --Wormwood alias & fallback table
 local fallback_wormwood = {
 	"emoji",
 	"controllers",
-	GLOBAL.TALKINGFONT_WORMWOOD,
-	GLOBAL.FALLBACK_FONT_OUTLINE,
-	GLOBAL.FALLBACK_FONT_FULL_OUTLINE,
+	_G.TALKINGFONT_WORMWOOD,
+	_G.FALLBACK_FONT_OUTLINE,
+	_G.FALLBACK_FONT_FULL_OUTLINE,
 }
-local TALKINGFONT_WORMWOOD_KR = "talkingfont_wormwood_kr"
+_G.TALKINGFONT_WORMWOOD_KR = "talkingfont_wormwood_kr"
 
 --Crabby Hermit alias & fallback table
 local fallback_hermit = {
 	"emoji",
 	"controllers",
-	GLOBAL.TALKINGFONT_HERMIT,
-	GLOBAL.FALLBACK_FONT_OUTLINE,
-	GLOBAL.FALLBACK_FONT_FULL_OUTLINE,
+	_G.TALKINGFONT_HERMIT,
+	_G.FALLBACK_FONT_OUTLINE,
+	_G.FALLBACK_FONT_FULL_OUTLINE,
 }
-local TALKINGFONT_HERMIT_KR = "talkingfont_hermit_kr"
+_G.TALKINGFONT_HERMIT_KR = "talkingfont_hermit_kr"
 
 --WX-78 alias & fallback table
-local TALKINGFONT_WX78_KR = "talkingfont_wx78_kr"
+_G.TALKINGFONT_WX78_KR = "talkingfont_wx78_kr"
 
 --Tradein alias & fallback table
 local fallback_tradein = {
 	"emoji",
 	"controllers",
-	GLOBAL.TALKINGFONT_TRADEIN,
-	GLOBAL.FALLBACK_FONT_OUTLINE,
-	GLOBAL.FALLBACK_FONT_FULL_OUTLINE,
+	_G.TALKINGFONT_TRADEIN,
+	_G.FALLBACK_FONT_OUTLINE,
+	_G.FALLBACK_FONT_FULL_OUTLINE,
 }
-local TALKINGFONT_TRADEIN_KR = "talkingfont_tradein_kr"
+_G.TALKINGFONT_TRADEIN_KR = "talkingfont_tradein_kr"
 
---font table
-local fonts = {
-	{filename = MODROOT.."fonts/talkingfont_wormwood_kr.zip", alias = TALKINGFONT_WORMWOOD_KR, fallback = fallback_wormwood},
-	{filename = MODROOT.."fonts/talkingfont_hermit_kr.zip", alias = TALKINGFONT_HERMIT_KR, fallback = fallback_hermit},
-	{filename = MODROOT.."fonts/talkingfont_wx78_kr.zip", alias = TALKINGFONT_WX78_KR, fallback = GLOBAL.DEFAULT_FALLBACK_TABLE_OUTLINE},
-	{filename = MODROOT.."fonts/talkingfont_tradein_kr.zip", alias = TALKINGFONT_TRADEIN_KR, fallback = fallback_tradein},
-}
-
---loads font
-for i, v in ipairs(fonts) do
-	table.insert(GLOBAL.FONTS, v)
-end
+--font loading
+AddSimPostInit(function()
+	TheSim:UnloadFont(_G.resolvefilepath("fonts/talkingfont_wormwood_kr.zip"), _G.TALKINGFONT_WORMWOOD_KR)
+	TheSim:UnloadFont(_G.resolvefilepath("fonts/talkingfont_wx78_kr.zip"), _G.TALKINGFONT_WX78_KR)
+	TheSim:UnloadFont(_G.resolvefilepath("fonts/talkingfont_hermit_kr.zip"), _G.TALKINGFONT_HERMIT_KR)
+	TheSim:UnloadFont(_G.resolvefilepath("fonts/talkingfont_tradein_kr.zip"), _G.TALKINGFONT_TRADEIN_KR)
+	
+	TheSim:UnloadPrefabs({"fontprefab"})
+	
+	local Assets = {
+		Asset("FONT", _G.resolvefilepath("fonts/talkingfont_wormwood_kr.zip")),
+		Asset("FONT", _G.resolvefilepath("fonts/talkingfont_hermit_kr.zip")),
+		Asset("FONT", _G.resolvefilepath("fonts/talkingfont_wx78_kr.zip")),
+		Asset("FONT", _G.resolvefilepath("fonts/talkingfont_tradein_kr.zip")),
+	}
+	local FontsPrefab = _G.Prefab("fontprefab", function() return _G.CreateEntity() end, Assets)
+	_G.RegisterPrefabs(FontsPrefab)
+	
+	TheSim:LoadPrefabs({"fontprefab"})
+	
+    TheSim:LoadFont(_G.resolvefilepath("fonts/talkingfont_wormwood_kr.zip"), _G.TALKINGFONT_WORMWOOD_KR)
+	TheSim:LoadFont(_G.resolvefilepath("fonts/talkingfont_wx78_kr.zip"), _G.TALKINGFONT_WX78_KR)
+	TheSim:LoadFont(_G.resolvefilepath("fonts/talkingfont_hermit_kr.zip"), _G.TALKINGFONT_HERMIT_KR)
+	TheSim:LoadFont(_G.resolvefilepath("fonts/talkingfont_tradein_kr.zip"), _G.TALKINGFONT_TRADEIN_KR)
+	
+	TheSim:SetupFontFallbacks(_G.TALKINGFONT_WORMWOOD_KR, fallback_wormwood)
+	TheSim:SetupFontFallbacks(_G.TALKINGFONT_WX78_KR, _G.DEFAULT_FALLBACK_TABLE_OUTLINE)
+	TheSim:SetupFontFallbacks(_G.TALKINGFONT_HERMIT_KR, fallback_hermit)
+	TheSim:SetupFontFallbacks(_G.TALKINGFONT_TRADEIN_KR, fallback_tradein)
+end)
 
 AddPrefabPostInit("wormwood", function(inst)
     inst.components.talker.fontsize = 35
-    inst.components.talker.font = TALKINGFONT_WORMWOOD_KR
+    inst.components.talker.font = _G.TALKINGFONT_WORMWOOD_KR
 end)
 
 AddPrefabPostInit("hermitcrab", function(inst)
     inst.components.talker.fontsize = 40
-    inst.components.talker.font = TALKINGFONT_HERMIT_KR
+    inst.components.talker.font = _G.TALKINGFONT_HERMIT_KR
 end)
 
 AddPrefabPostInit("wx78", function(inst)
     inst.components.talker.fontsize = 30
-    inst.components.talker.font = TALKINGFONT_WX78_KR
+    inst.components.talker.font = _G.TALKINGFONT_WX78_KR
 end)
 
 AddPrefabPostInit("yotb_stage", function(inst)
-    inst.components.talker.font = TALKINGFONT_TRADEIN_KR
+    inst.components.talker.font = _G.TALKINGFONT_TRADEIN_KR
 end)
 
 AddPrefabPostInit("yotb_stage_voice", function(inst)
-    inst.components.talker.font = TALKINGFONT_TRADEIN_KR
+    inst.components.talker.font = _G.TALKINGFONT_TRADEIN_KR
 end)

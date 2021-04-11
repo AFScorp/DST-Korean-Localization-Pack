2e79904e9c4ccd85f816eb3f310615838874cb3a
AFS Co. Ltd <qkrcotlr1@gmail.com>
2021-04-11 21:39:56 +0900

    당근이쥐 경주 조사 처리

--- modmains/modmain.lua
+++ modmains/modmain.lua
@@ -241,7 +241,22 @@ end
 	
 
 --pp. handling for Carrat Race
-
+local function getdesc(inst, viewer)
+	if inst:HasTag("burnt") then
+		return GetDescription(viewer, inst, "BURNT")
+	elseif inst._active and inst._winner ~= nil then
+		if inst._winner.userid ~= nil and inst._winner.userid == viewer.userid then
+			return GetDescription(viewer, inst, "I_WON")
+		elseif inst._winner.name ~= nil then
+			return subfmt(pp.replacePP(GetDescription(viewer, inst, "SOMEONE_ELSE_WON"), "{winner}", inst._winner.name), { winner = inst._winner.name })
+		end
+	end
+	
+	return GetDescription(viewer, inst) or nil
+end
+AddPrefabPostInit("yotc_carrat_race_finish", function(inst)
+	inst.components.inspectable.getspecialdescription = getdesc
+end)
 
 -- In-Game UI Clock
 AddClassPostConstruct("widgets/uiclock", function(self)
